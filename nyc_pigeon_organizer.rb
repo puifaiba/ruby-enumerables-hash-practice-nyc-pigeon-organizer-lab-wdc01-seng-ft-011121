501680f9357b4ebc94c28fde869d8f7b8e564df6
Boonie Attawut <puifai.ba@gmail.com>
2021-01-04 23:57:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = data.each_with_object ({}) do |(key, value), result_array|
+    value.each do |attribute, names|
+      names.each do |name|
+        if !result_array[name]
+          result_array[name] = {}
+        end
+        if !result_array[name][key]
+          !result_array[name][key] = []
+        end
+        result_array[name][key].push(attribute.to_s)
+      end
+    end
+  end
 end
